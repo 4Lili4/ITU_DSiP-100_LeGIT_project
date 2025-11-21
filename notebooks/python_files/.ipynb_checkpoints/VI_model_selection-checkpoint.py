fbf2b57b8a9d55c8898afba7a2f1e21aa9153009
lilka <Lilja.kaeregaard@gmail.com>
2025-11-21 14:02:58 +0100

    style: reformatting python-files
    Co-Authored-By: Wenzel Keil <158325841+Chopopo@users.noreply.github.com>

---
+++ notebooks/python_files/.ipynb_checkpoints/VI_model_selection-checkpoint.py
@@ -0,0 +1,68 @@
+import mlflow
+import json
+
+def get_exp_model_res():
+    experiment_ids = [mlflow.get_experiment_by_name(experiment_name).experiment_id]
+    experiment_best = mlflow.search_runs(
+        experiment_ids=experiment_ids,
+        order_by=["metrics.f1_score DESC"],
+        max_results=1
+    ).iloc[0]
+    
+    with open("./artifacts/model_results.json", "r") as f:
+        model_results = json.load(f)
+    results_df = pd.DataFrame({model: val["weighted avg"] for model, val in model_results.items()}).T
+    
+    best_model = results_df.sort_values("f1-score", ascending=False).iloc[0].name
+    print(f"Best model: {best_model}")
+
+def get_production_model():
+    client = MlflowClient()
+    prod_model = [model for model in client.search_model_versions(f"name='{model_name}'") if dict(model)['current_stage']=='Production']
+    prod_model_exists = len(prod_model)>0
+    
+    if prod_model_exists:
+        prod_model_version = dict(prod_model[0])['version']
+        prod_model_run_id = dict(prod_model[0])['run_id']
+        
+        print('Production model name: ', model_name)
+        print('Production model version:', prod_model_version)
+        print('Production model run id:', prod_model_run_id)
+        
+    else:
+        print('No model in production')
+
+def comp_models():
+    train_model_score = experiment_best["metrics.f1_score"]
+    model_details = {}
+    model_status = {}
+    run_id = None
+    
+    if prod_model_exists:
+        data, details = mlflow.get_run(prod_model_run_id)
+        prod_model_score = data[1]["metrics.f1_score"]
+    
+        model_status["current"] = train_model_score
+        model_status["prod"] = prod_model_score
+    
+        if train_model_score>prod_model_score:
+            print("Registering new model")
+            run_id = experiment_best["run_id"]
+    else:
+        print("No model in production")
+        run_id = experiment_best["run_id"]
+    
+    print(f"Registered model: {run_id}")
+
+def reg_best_model():
+    if run_id is not None:
+        print(f'Best model found: {run_id}')
+    
+        model_uri = "runs:/{run_id}/{artifact_path}".format(
+            run_id=run_id,
+            artifact_path=artifact_path
+        )
+        model_details = mlflow.register_model(model_uri=model_uri, name=model_name)
+        wait_until_ready(model_details.name, model_details.version)
+        model_details = dict(model_details)
+        print(model_details)
