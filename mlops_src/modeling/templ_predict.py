8707451ab992d5c082894dca2fcd3210f9e07d1e
Chopopo <weke@itu.dk>
2025-12-10 11:01:38 +0900

    fix: import error

--- mlops_src/modeling/templ_predict.py
+++ mlops_src/modeling/templ_predict.py
@@ -4,11 +4,15 @@ from loguru import logger
 from tqdm import tqdm
 import typer
 
-from mlops_src.templ_config import MODELS_DIR, PROJ_ROOT, PROCESSED_DATA_DIR
-
 import sklearn
 import pandas as pd
 import joblib
+import os, sys
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+sys.path.append(project_root)
+from mlops_src.templ_config import MODELS_DIR, PROCESSED_DATA_DIR, DATA_DIR
+
 
 app = typer.Typer()
 
@@ -16,7 +20,7 @@ app = typer.Typer()
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROJ_ROOT / "/external/test_features.csv",
+    features_path: Path = DATA_DIR / "external/X_test.csv",
     model_path: Path = MODELS_DIR / "lead_model_lr.pkl",
     predictions_path: Path = PROCESSED_DATA_DIR / "test_predictions.csv",
     # -----------------------------------------
